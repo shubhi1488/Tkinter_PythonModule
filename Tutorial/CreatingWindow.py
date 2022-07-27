ce003fb9da91cff5fd0c340df3bf68b6425d5e01
Shubhi Agarwal <shubhi1488@gmail.com>
2022-07-27 20:24:22 +0530

    Create CreatingWindow.py

---
+++ Tutorial/CreatingWindow.py
@@ -0,0 +1,11 @@
+from tkinter import *
+#it will load all the basic components of GUI
+var_root=Tk()
+#it will set the size of the window that will appear
+var_root.geometry("444x234")
+#this method allows u to minimise the window upto a mentioned width and height
+var_root.minsize(200,100)
+#this method allows u to maximise the window upto a mentioned width and height
+var_root.maxsize(1200,988)
+#mainloop() will make the window respinsive and store all the work done .
+var_root.mainloop()
